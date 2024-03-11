02332d25140c6cfc666f80ff8eb423c2b975f019
aswin0221 <aswincuts123@gmail.com>
2024-03-11 16:24:00 +0530

    popscope added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -95,83 +95,86 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<ScheduleProvider>(
-      builder: (context, ScheduleProvider, child) => Scaffold(
-        appBar: AppBar(
-          title: const Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(Icons.alarm ,color: Colors.white,),
-              SizedBox(width: 10,),
-              Text("Daily Remainder",style: TextStyle(color: Colors.white,fontWeight: FontWeight.w500),),
-            ],
-          ),
-          centerTitle: true,
-          backgroundColor: const Color(0xff3475db),
-          elevation: 0,
-           toolbarHeight: MediaQuery.sizeOf(context).height*0.12,
-           shape: const RoundedRectangleBorder(
-             borderRadius: BorderRadius.vertical(
-               bottom: Radius.circular(30),
-             ),
-           ),
-          scrolledUnderElevation: 0,
-        ),
-        body: Container(
-          width: MediaQuery.sizeOf(context).width,
-          padding: const EdgeInsets.only(top: 20 , left: 20 , right: 20),
-          child: Column(
-             mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
+    return PopScope(
+      canPop: false,
+      child: Consumer<ScheduleProvider>(
+        builder: (context, ScheduleProvider, child) => Scaffold(
+          appBar: AppBar(
+            title: const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                if(!ScheduleProvider.notificationIsAllowed)
-                  GestureDetector(
-                    onTap: (){
-                      diologbox();
-                      ScheduleProvider.checkSessionAndNavigate();
-                    },
-                    child: Container(
-                      padding: const EdgeInsets.symmetric(horizontal: 5,vertical: 5),
-                      width: MediaQuery.of(context).size.width,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10),
-                        color: Colors.grey[200]
-                      ),
-                      child:
-                      const Text("Notifications is Disabled Click Here to Enable Notifications",textAlign: TextAlign.center , style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),),
-                    ),
-                  ),
-                const SizedBox(height: 10,),
-                ScheduleProvider.remainderList.isEmpty
-                    ? const Center(child: Text("No Works to do !!" , style: TextStyle(color: Colors.black54,fontSize: 18 , fontWeight:FontWeight.bold),))
-                    : const Text(
-                  "Upcoming Works",
-                  style: TextStyle(color: Colors.black54,fontSize: 18 , fontWeight:FontWeight.bold),
-                ),
-               // Text("Upcoming Works",style: TextStyle(color: Colors.black54,fontSize: 18 , fontWeight:FontWeight.bold),),
-                const SizedBox(height: 10,),
-                Expanded(
-                  child: ListView.builder(
-                      itemCount: ScheduleProvider.remainderList.length,
-                      itemBuilder: (context,index)
-                  {
-                    return UpcomingWorks(title: ScheduleProvider.remainderList[index].title ?? "",dateTime: ScheduleProvider.remainderList[index].dateTime ?? 1, discreption: ScheduleProvider.remainderList[index].description ?? "",);
-                  }
-                  ),
-                )
-            ],
+                Icon(Icons.alarm ,color: Colors.white,),
+                SizedBox(width: 10,),
+                Text("Daily Remainder",style: TextStyle(color: Colors.white,fontWeight: FontWeight.w500),),
+              ],
+            ),
+            centerTitle: true,
+            backgroundColor: const Color(0xff3475db),
+            elevation: 0,
+             toolbarHeight: MediaQuery.sizeOf(context).height*0.12,
+             shape: const RoundedRectangleBorder(
+               borderRadius: BorderRadius.vertical(
+                 bottom: Radius.circular(30),
+               ),
+             ),
+            scrolledUnderElevation: 0,
           ),
+          body: Container(
+            width: MediaQuery.sizeOf(context).width,
+            padding: const EdgeInsets.only(top: 20 , left: 20 , right: 20),
+            child: Column(
+               mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if(!ScheduleProvider.notificationIsAllowed)
+                    GestureDetector(
+                      onTap: (){
+                        diologbox();
+                        ScheduleProvider.checkSessionAndNavigate();
+                      },
+                      child: Container(
+                        padding: const EdgeInsets.symmetric(horizontal: 5,vertical: 5),
+                        width: MediaQuery.of(context).size.width,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
+                          color: Colors.grey[200]
+                        ),
+                        child:
+                        const Text("Notifications is Disabled Click Here to Enable Notifications",textAlign: TextAlign.center , style: TextStyle(fontSize: 12, fontWeight: FontWeight.w500),),
+                      ),
+                    ),
+                  const SizedBox(height: 10,),
+                  ScheduleProvider.remainderList.isEmpty
+                      ? const Center(child: Text("No Works to do !!" , style: TextStyle(color: Colors.black54,fontSize: 18 , fontWeight:FontWeight.bold),))
+                      : const Text(
+                    "Upcoming Works",
+                    style: TextStyle(color: Colors.black54,fontSize: 18 , fontWeight:FontWeight.bold),
+                  ),
+                 // Text("Upcoming Works",style: TextStyle(color: Colors.black54,fontSize: 18 , fontWeight:FontWeight.bold),),
+                  const SizedBox(height: 10,),
+                  Expanded(
+                    child: ListView.builder(
+                        itemCount: ScheduleProvider.remainderList.length,
+                        itemBuilder: (context,index)
+                    {
+                      return UpcomingWorks(title: ScheduleProvider.remainderList[index].title ?? "",dateTime: ScheduleProvider.remainderList[index].dateTime ?? 1, discreption: ScheduleProvider.remainderList[index].description ?? "",);
+                    }
+                    ),
+                  )
+              ],
+            ),
+          ),
+           floatingActionButton: FloatingActionButton(
+         backgroundColor: const Color(0xff3475db),
+             child: const Icon(Icons.add,color: Colors.white,),
+             onPressed: () async{
+               await ScheduleProvider.checkSessionAndNavigate();
+                Navigator.push(context, PageTransition(child: AddEvent(), type: PageTransitionType.rightToLeft )).then((data) => {
+               // getAllUsers()
+             });
+           },
+           ),
         ),
-         floatingActionButton: FloatingActionButton(
-       backgroundColor: const Color(0xff3475db),
-           child: const Icon(Icons.add,color: Colors.white,),
-           onPressed: () async{
-             await ScheduleProvider.checkSessionAndNavigate();
-              Navigator.push(context, PageTransition(child: AddEvent(), type: PageTransitionType.rightToLeft )).then((data) => {
-             // getAllUsers()
-           });
-         },
-         ),
       ),
     );
   }
